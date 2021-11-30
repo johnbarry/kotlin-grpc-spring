01ef022c0f63f463283f539e06d88cf1e2c30b54
johnbarry <john.barry@me.com>
2021-11-29 20:59:02 -0500

    First cut writing Command to Kafka

--- server/src/main/kotlin/io/grpc/examples/helloworld/KafkaMessageWriter.kt
+++ server/src/main/kotlin/io/grpc/examples/helloworld/KafkaMessageWriter.kt
@@ -15,7 +15,7 @@ import reactor.kafka.sender.SenderRecord
 class KafkaMessageWriter(private val topicName: String) {
 
     private val props = mapOf(
-        Pair(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"),
+        Pair(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092"),
         Pair(ProducerConfig.CLIENT_ID_CONFIG, "command-writer"),
         Pair(ProducerConfig.ACKS_CONFIG, "all"),
         Pair(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java),
@@ -25,14 +25,15 @@ class KafkaMessageWriter(private val topicName: String) {
 
     fun write(kv: Flux<Pair<String, String>>) =
         sender.send<String> {
-            kv
-                .map {
+            kv.map {
                     SenderRecord.create(ProducerRecord(topicName, it.first, it.second), it.first)
                 }
-                .doOnError { log.error("Kafka send failed", it) }
-                .subscribe { log.info("Sent record ${it.correlationMetadata()}")
-                }
         }
+            .log()
+            .doOnError { log.error("Kafka send failed", it) }
+            .subscribe {
+                log.info("Sent record ${it.correlationMetadata()}")
+            }
 
     companion object {
         val log: Logger = LoggerFactory.getLogger(KafkaMessageWriter::class.java)
