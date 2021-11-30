8fbee70ddf815bac628c046933f43944f6558979
johnbarry <john.barry@me.com>
2021-11-29 23:42:07 -0500

    First cut writing Command to Kafka

--- server/src/main/kotlin/io/grpc/examples/helloworld/KafkaMessageWriter.kt
+++ server/src/main/kotlin/io/grpc/examples/helloworld/KafkaMessageWriter.kt
@@ -1,8 +1,6 @@
 package io.grpc.examples.helloworld
 
-import com.google.protobuf.util.JsonFormat
 import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -14,26 +12,30 @@ import reactor.kafka.sender.SenderRecord
 
 class KafkaMessageWriter(private val topicName: String) {
 
-    private val props = mapOf(
-        Pair(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092"),
+    private val props: Map<String, Any> = mapOf(
+        Pair(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"),
         Pair(ProducerConfig.CLIENT_ID_CONFIG, "command-writer"),
-        Pair(ProducerConfig.ACKS_CONFIG, "all"),
+        Pair(ProducerConfig.ACKS_CONFIG, "1"),
         Pair(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java),
         Pair(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
     )
-    private val sender: KafkaSender<String, String> = KafkaSender.create(SenderOptions.create<String, String>(props))
+    private val sender: KafkaSender<String, String> = KafkaSender.create(SenderOptions.create(props))
 
-    fun write(kv: Flux<Pair<String, String>>) =
-        sender.send<String> {
-            kv.map {
-                    SenderRecord.create(ProducerRecord(topicName, it.first, it.second), it.first)
-                }
+    fun write(kv: Flux<Pair<String, String>>) {
+        log.info("Start kafka write...")
+        val newFlux = kv.map {
+            SenderRecord.create(topicName,0, System.currentTimeMillis(), it.first, it.second, it.first)
         }
-            .log()
-            .doOnError { log.error("Kafka send failed", it) }
-            .subscribe {
-                log.info("Sent record ${it.correlationMetadata()}")
+        sender.send (newFlux)
+            .doOnError {
+                log.error("Kafka send failed: {}", it)
             }
+            .subscribe {
+                log.info("Kafka write result correlation = {}", it.correlationMetadata())
+
+            }
+        log.info("Completed kafka write")
+    }
 
     companion object {
         val log: Logger = LoggerFactory.getLogger(KafkaMessageWriter::class.java)
